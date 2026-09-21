598715b616559d6237bd97be85e658ef25752936
felicitousbeing999-h <felicitousbeing888@gmail.com>
2026-09-21 16:24:05 +0530

    Create main.tf

---
+++ advanced/t1/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # Adjust to your target AWS region if required by your lab
+}
+resource "aws_dynamodb_table" "nautilus_table" {
+  name         = var.KKE_TABLE_NAME
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "nautilus_role" {
+  name = var.KKE_ROLE_NAME
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "nautilus_readonly_policy" {
+  name        = var.KKE_POLICY_NAME
+  description = "Read-only access to ${var.KKE_TABLE_NAME}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:Scan",
+          "dynamodb:Query",
+          "dynamodb:BatchGetItem",
+          "dynamodb:DescribeTable"
+        ]
+        Resource = [
+          aws_dynamodb_table.nautilus_table.arn,
+          "${aws_dynamodb_table.nautilus_table.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "nautilus_role_attach" {
+  role       = aws_iam_role.nautilus_role.name
+  policy_arn = aws_iam_policy.nautilus_readonly_policy.arn
+}
