988725e6e920e771224447c1ee7bddd28d95857b
felicitousbeing999-h <felicitousbeing888@gmail.com>
2026-09-21 16:24:25 +0530

    Create variables.tf

---
+++ advanced/t1/variables.tf
@@ -0,0 +1,13 @@
+variable "KKE_TABLE_NAME"{
+    description="NAME OF DYNAMODB TABLE"
+    type=string
+}
+
+variable "KKE_ROLE_NAME"{
+    description="NAME OF THE IAM ROLE"
+    type=string
+}
+variable "KKE_POLICY_NAME"{
+    description="NAME OF THE IAM POLICY"
+    type=string
+}
